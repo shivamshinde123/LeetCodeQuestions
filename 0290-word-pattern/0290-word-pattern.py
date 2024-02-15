c581607f41f87c6c3d3505ed2188eee04c9d14c7
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-02-15 17:16:51 +0530

    Time: 41 ms (25.04%), Space: 16.8 MB (52.09%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,5 +1,7 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
+        
+        # Approach 1
         pattern_list = list()
         str_list = list()
         
@@ -10,3 +12,25 @@ class Solution:
             str_list.append(s.split().index(item))
             
         return pattern_list == str_list
+    
+        # Approach 2
+        connections = dict()
+        words = s.split()
+        
+        if len(pattern) != len(words):
+            return False
+        
+        if len(set(pattern)) != len(set(words)):
+            return False
+        
+        
+        for i in range(len(words)):
+            if words[i] in connections:
+                if connections[words[i]] != pattern[i]:
+                    return False
+                
+            else:
+                connections[words[i]] = p[i]
+                
+        return True
+        
