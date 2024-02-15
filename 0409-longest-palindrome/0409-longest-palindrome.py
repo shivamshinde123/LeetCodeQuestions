10460059e25e20548672d475efca83fa0fb1033e
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-02-15 13:23:11 +0530

    Time: 45 ms (17.12%), Space: 16.6 MB (67.28%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -9,18 +9,19 @@ class Solution:
         for char in s:
             count[char] = count.get(char,0) + 1
             
-        even_val = list()
+        even_val_sum = 0
         odd_val = list()
         
         for val in count.values():
             if val % 2 == 0:
-                even_val.append(val)
+                even_val_sum += val
             else:
                 odd_val.append(val)
                 
+        longest_palin = even_val_sum
+        
         odd_val.sort(reverse=True)
-                
-        longest_palin = sum(even_val)
+        
         if len(odd_val) == 1:
             longest_palin = longest_palin + odd_val[0]
         elif len(odd_val) > 1:
