8a654aa4864360abec9e62e632854ae66e709e4c
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-02-15 14:24:14 +0530

    Time: 248 ms (86.50%), Space: 27.3 MB (41.91%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -5,16 +5,26 @@ class Solution:
         # n = len(nums)
         # return list(set(list(range(n,0,-1))).difference(nums))
         
-        freq = {i:0 for i in range(1,len(nums)+1)}
         
-        for elem in nums:
-            freq[elem] += 1
+#         Approach 2: using dict
+#         freq = {i:0 for i in range(1,len(nums)+1)}
+        
+#         for elem in nums:
+#             freq[elem] += 1
             
+#         answer = list()
+#         for key in freq.keys():
+#             if freq[key] == 0:
+#                 answer.append(key)
+                
+#         return answer
+            
+        # Using set
+        unique_elems = set(nums)
+        
         answer = list()
-        for key in freq.keys():
-            if freq[key] == 0:
-                answer.append(key)
+        for i in range(1, len(nums)+1):
+            if i not in unique_elems:
+                answer.append(i)
                 
         return answer
-            
-        
