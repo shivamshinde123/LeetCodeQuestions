8a96f5b53f875ca43010d152b571923c2a7449c8
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-02-16 14:39:00 +0530

    Time: 198 ms (34.27%), Space: 17.9 MB (89.91%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.py
+++ 0697-degree-of-an-array/0697-degree-of-an-array.py
@@ -1,29 +1,51 @@
 class Solution:
     def findShortestSubArray(self, nums: List[int]) -> int:
         
-        freq = dict()
+        # Approach 1: 
+#         freq = dict()
         
-        for elem in nums:
-            freq[elem] = freq.get(elem, 0) + 1
+#         for elem in nums:
+#             freq[elem] = freq.get(elem, 0) + 1
             
-        degree = max(freq.values())
+#         degree = max(freq.values())
 
-        keys_of_degree = list()
-        for key, value in freq.items():
-            if value == degree:
-                keys_of_degree.append(key)
+#         keys_of_degree = list()
+#         for key, value in freq.items():
+#             if value == degree:
+#                 keys_of_degree.append(key)
                 
-        def Dist(elem):
-            arr = list()
-            for i, num in enumerate(nums):
-                if num == elem:
-                    arr.append(i)
+#         def Dist(elem):
+#             arr = list()
+#             for i, num in enumerate(nums):
+#                 if num == elem:
+#                     arr.append(i)
                     
-            return arr[-1] - arr[0] + 1
+#             return arr[-1] - arr[0] + 1
         
-        shortestDistance = math.inf
-        for key in keys_of_degree:
-            shortestDistance = min(shortestDistance, Dist(key))
+#         shortestDistance = math.inf
+#         for key in keys_of_degree:
+#             shortestDistance = min(shortestDistance, Dist(key))
             
-        return shortestDistance
+#         return shortestDistance
+
+
+        # Approach 2    
+        first_occurence_index = dict()
+        freq = dict()
+        result = 0
+        degree = 0
+        
+        for i, num in enumerate(nums):
+            first_occurence_index.setdefault(num, i)
+            freq[num] = freq.get(num, 0) + 1
+            
+            if freq[num] > degree:
+                degree = freq[num]
+                result = i - first_occurence_index[num] + 1
+            elif freq[num] == degree:
+                result = min(result, i-first_occurence_index[num]+1)
+            
+        return result
+    
+        
             
