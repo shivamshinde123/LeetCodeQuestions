ef52f45ed479a0b77c286b8743c63529e0af13f1
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-02-19 12:35:11 +0530

    Time: 39 ms (32.12%), Space: 16.6 MB (67.47%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.py
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -1,13 +1,27 @@
 class Solution:
     def findWords(self, words: List[str]) -> List[str]:
-        
-        first_row = "qwertyuiop"
-        second_row = "asdfghjkl"
-        third_row = "zxcvbnm"
-        
+
+        ## Approach 1
+#         first_row = "qwertyuiop"
+#         second_row = "asdfghjkl"
+#         third_row = "zxcvbnm"
+#         answer = []
+#         for word in words:
+#             if all(char in first_row for char in word.lower()) or all(char in second_row for char in word.lower()) or all(char in third_row for char in word.lower()):
+#                 answer.append(word)
+                
+#         return answer
+
+        # Approach 2
+        first_row = set("qwertyuiop")
+        second_row = set("asdfghjkl")
+        third_row = set("zxcvbnm")
         answer = []
         for word in words:
-            if all(char in first_row for char in word.lower()) or all(char in second_row for char in word.lower()) or all(char in third_row for char in word.lower()):
+            w = set(word.lower())
+            
+            if w.issubset(first_row) or w.issubset(second_row) or w.issubset(third_row):
                 answer.append(word)
                 
         return answer
+            
