6927aa19a5cae8e8e8961070fc58bba052ca3c10
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-02-19 10:51:54 +0530

    Time: 30 ms (90.28%), Space: 16.7 MB (53.28%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,10 +1,11 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         
-        if (n % 2 == 1 and n > 1) or (n==0):
+        if n > 1 and n % 2 == 1:
             return False
-        
-        if n == 1:
+        elif n == 0:
+            return False
+        elif n == 1:
             return True
         
         if self.isPowerOfTwo(n/2):
