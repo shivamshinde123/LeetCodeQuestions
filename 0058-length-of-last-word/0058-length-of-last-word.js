73b0e3673eaf755a2e5650008f6ea04ebe5f66c9
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-02-19 17:55:09 +0530

    Time: 45 ms (88.21%), Space: 50.2 MB (5.60%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -8,9 +8,12 @@ var lengthOfLastWord = function(s) {
     // let words =  s.trim().split(" ")
     // let last_word = words[words.length-1]
     // return last_word.length
-    words = s.trim().split(" ")
+
+    // Approach 2
+    words = s.split(" ")
+    console.log(words)
     for (let i = words.length-1; i>-1; i--){
-        if (words[i] != " "){
+        if (words[i] != ""){
             return words[i].length
         }
     }
