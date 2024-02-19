0973cba2be5977dcb57f5590dd1a344bc6dd8f04
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-02-19 13:52:13 +0530

    Time: 235 ms (30.57%), Space: 17.1 MB (89.53%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -1,22 +1,40 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
         
-        common_strings = dict()
+        # Approach 1
+#         common_strings = dict()
+        
+#         for i in range(len(list1)):
+#             if list1[i] in list2:
+#                 j = list2.index(list1[i])
+#                 common_strings[list1[i]] = i + j
+                
+#         min_index_sum = min(common_strings.values())
+        
+        
+#         answer = list()
+#         for string in common_strings:
+#             if common_strings[string] == min_index_sum:
+#                 answer.append(string)
+                
+#         return answer
+
+        # Appraoch 2
+        index_sum = math.inf
+        common_strings = list()
+        
         
         for i in range(len(list1)):
             if list1[i] in list2:
                 j = list2.index(list1[i])
-                common_strings[list1[i]] = i + j
-                
-        min_index_sum = min(common_strings.values())
-        
-        
-        answer = list()
-        for string in common_strings:
-            if common_strings[string] == min_index_sum:
-                answer.append(string)
-                
-        return answer
+                if i + j < index_sum:
+                    index_sum = i + j
+                    common_strings.clear()
+                    common_strings.append(list1[i])
+                elif i + j == index_sum:
+                    common_strings.append(list1[i])
+                    
+        return common_strings
                 
         
                     
