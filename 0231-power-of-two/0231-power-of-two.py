a041654e08a73a1a462301144c286b41b9230cdd
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-02-19 11:01:07 +0530

    Time: 35 ms (69.97%), Space: 16.4 MB (96.02%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,14 +1,20 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         
-        if n > 1 and n % 2 == 1:
-            return False
-        elif n == 0:
-            return False
-        elif n == 1:
-            return True
+        # Approach 1: recursive approach
+#         if n > 1 and n % 2 == 1:
+#             return False
+#         elif n == 0:
+#             return False
+#         elif n == 1:
+#             return True
         
-        if self.isPowerOfTwo(n/2):
-            return True
+#         if self.isPowerOfTwo(n/2):
+#             return True
         
+        # Approach 2: bitwise and operation (faster)
+        # Following is true when n is the power of two:
+        # pow(2, n) & (pow(2, n) - 1) == 0 
+        
+        return n and not(n & (n-1))
         
