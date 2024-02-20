a39edd8a70ce8af7f6f43a467c3322891dc02e08
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-02-20 15:55:40 +0530

    Time: 39 ms (86.62%), Space: 16.8 MB (83.12%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -1,9 +1,9 @@
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
         scores = list()
-        
+        is_numeric = lambda x: x.replace('.', '', 1).replace('-', '', 1).isdigit()
         for i in range(len(operations)):
-            if operations[i].lstrip('-').isdigit():
+            if is_numeric(operations[i]):
                 scores.append(int(operations[i]))
             elif operations[i] == '+':
                 scores.append(int(scores[-1]) + int(scores[-2]))
@@ -11,6 +11,5 @@ class Solution:
                 scores.append(2*int(scores[-1]))
             else:
                 scores.pop()
-            print(scores)
         return sum(scores)
                 
