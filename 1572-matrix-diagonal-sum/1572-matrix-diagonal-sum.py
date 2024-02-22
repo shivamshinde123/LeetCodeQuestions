e91631dff103de7e784498cf8127fc5ce9d64feb
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-02-22 14:16:08 +0530

    Time: 106 ms (16.57%), Space: 16.8 MB (81.86%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -1,11 +1,23 @@
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
         
+        # Approach 1
         n = len(mat)
         required_sum = 0
-        
         for i in range(n):
-            required_sum += mat[i][i]
-            required_sum += mat[i][n-1-i]
+            for j in range(n):
+                if i == j or i == n - 1- j:
+                    required_sum += mat[i][j]
+                    
+        return required_sum
+        
+        # Approach 2
+#         n = len(mat)
+#         required_sum = 0
+        
+#         for i in range(n):
+#             required_sum += mat[i][i]
+#             required_sum += mat[i][n-1-i]
             
-        return required_sum if n % 2 == 0 else required_sum - mat[n//2][n//2]
+#         # removing the value that is added twice which is present only when mat have odd number of rows
+#         return required_sum if n % 2 == 0 else required_sum - mat[n//2][n//2]
