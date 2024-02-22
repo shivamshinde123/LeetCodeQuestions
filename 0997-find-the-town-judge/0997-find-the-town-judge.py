b99fec2ff7f51bb32fa347b673c64722a9a2a6a9
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-02-22 11:29:02 +0530

    Time: 593 ms (85.10%), Space: 21.4 MB (95.97%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -12,10 +12,10 @@ class Solution:
             townspeople.add(t[0])
             trust_freq[t[1]] = trust_freq.get(t[1],0) + 1
             
-        trust_freq_reversed =  {value:key for key,value in trust_freq.items()} # how many people trust him: person
+        # trust_freq_reversed =  {value:key for key,value in trust_freq.items()} # how many people trust him: person
         
         # all the people trust the potential judge i.e. n -1 people, and if judge is not present then we will get -1
-        potential_judge = trust_freq_reversed.get(n-1,-1) 
+        potential_judge = {value:key for key,value in trust_freq.items()}.get(n-1,-1) 
         
         # potential judge doesn't trust townspeople
         if potential_judge != -1 and potential_judge not in townspeople:
