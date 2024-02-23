2ce5d3cfb7104b039e45e2fb922746a7df8fefde
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-02-23 13:18:51 +0530

    Time: 629 ms (52.26%), Space: 21.3 MB (51.79%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -1,24 +1,44 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
         
-        change = dict()
+        # Approach 1
+#         change = dict()
         
-        for bill in bills:
-            change[bill] = change.get(bill, 0) + 1
+#         for bill in bills:
+#             change[bill] = change.get(bill, 0) + 1
             
-            if bill == 10:
-                if change.get(5, 0) == 0:
-                    return False
-                elif change.get(5, 0) > 0:
-                    change[5] -= 1
+#             if bill == 10:
+#                 if change.get(5, 0) == 0:
+#                     return False
+#                 elif change.get(5, 0) > 0:
+#                     change[5] -= 1
                     
-            elif bill == 20:
-                if change.get(5, 0) >= 1 and change.get(10, 0) >= 1:
-                    change[5] -= 1
-                    change[10] -= 1
-                elif change.get(5, 0) >= 3:
-                    change[5] -= 3
+#             elif bill == 20:
+#                 if change.get(5, 0) >= 1 and change.get(10, 0) >= 1:
+#                     change[5] -= 1
+#                     change[10] -= 1
+#                 elif change.get(5, 0) >= 3:
+#                     change[5] -= 3
+#                 else:
+#                     return False
+        
+#         return True
+
+        # Approach 2
+        five = ten = 0
+        for bill in bills:
+            if bill == 5:
+                five += 1
+            elif bill == 10:
+                if not five: return False
+                five -= 1
+                ten += 1
+            else:
+                if ten and five:
+                    ten -= 1
+                    five -= 1
+                elif five >= 3:
+                    five -= 3
                 else:
                     return False
-        
         return True
