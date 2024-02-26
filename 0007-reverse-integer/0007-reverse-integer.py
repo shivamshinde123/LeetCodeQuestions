94717361d47f0d5331445ae69612493e587d3ccc
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-02-26 12:31:15 +0530

    Time: 29 ms (93.12%), Space: 16.5 MB (97.66%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,25 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        pos = x > 0
+        
+        if not pos:
+            x = -x 
+
+        # works with positive integers
+        def get_digit(x, index):
+            return x // (10**index) % 10
+        
+        reverse = 0
+        n = len(str(x))
+        
+        for i in range(n):
+            reverse += get_digit(x, i) * (10**(n-1-i))
+            
+        if reverse < -1*2**31 or reverse > (2**31-1):
+            return 0
+        
+        if pos:
+            return reverse
+        else:
+            return -1 * reverse
