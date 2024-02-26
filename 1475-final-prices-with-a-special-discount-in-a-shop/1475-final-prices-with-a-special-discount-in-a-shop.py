8db47f39e3ebbff1d3c49793357823c6f16ebc70
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-02-26 11:23:48 +0530

    Time: 64 ms (12.69%), Space: 16.6 MB (79.67%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -2,14 +2,31 @@ class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
         
         # Using a stack
-        stack = list()
+#         stack = list()
         
-        for i in range(len(prices)):
+#         for i in range(len(prices)):
             
-            while stack and prices[stack[-1]] >= prices[i]:
-                prices[stack.pop()] -= prices[i]
+#             while stack and prices[stack[-1]] >= prices[i]:
+#                 prices[stack.pop()] -= prices[i]
                 
-            stack.append(i)
+#             stack.append(i)
             
+#         return prices
+    
+        # Using an array
+        
+        def findLesserValue(index):
+            for val in prices[index+1:]:
+                if val <= prices[index]:
+                    return val
+            
+            return 0
+        
+        for i in range(len(prices)-1):
+            prices[i] -= findLesserValue(i)
+        
         return prices
+        
+        
+            
             
