bf62aabf5aae8b56506bdedde4ac6092e5eb472d
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-02-29 16:12:46 +0530

    Time: 682 ms (24.88%), Space: 16.6 MB (64.14%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,38 +1,65 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         
+        # Approach 1: Using threeSum method
+#         n = len(nums)
+#         output = list()
+#         nums.sort()
+        
+#         def threeSum(nums, target):
+#             answer = set()
+#             n = len(nums)
+            
+#             for i in range(n-1):
+
+#                 j = i + 1
+#                 k = n - 1
+
+#                 while j < k:
+#                     sum_ = nums[i] + nums[j] + nums[k]
+
+#                     if sum_ == target:
+#                         answer.add((nums[i], nums[j], nums[k]))
+#                         j += 1
+#                     elif sum_ > target:
+#                         k -= 1
+#                     else:
+#                         j += 1
+
+#             return list(answer)
+        
+#         for i in range(n-2):
+#             out = threeSum(nums[i+1:], target-nums[i])
+            
+#             for item in out:
+#                 if [nums[i]] + list(item) not in output:
+#                     output.append([nums[i]] + list(item))
+        
+#         return output
+    
+        # Approach 2: Intergrating threeSum into only one function
         n = len(nums)
-        output = list()
+        answer = set()
         nums.sort()
         
-        def threeSum(nums, target):
-            answer = set()
-            n = len(nums)
-            
-            for i in range(n-1):
-
-                j = i + 1
-                k = n - 1
-
-                while j < k:
-                    sum_ = nums[i] + nums[j] + nums[k]
-
-                    if sum_ == target:
-                        answer.add((nums[i], nums[j], nums[k]))
-                        j += 1
-                    elif sum_ > target:
-                        k -= 1
-                    else:
-                        j += 1
-
-            return list(answer)
-        
         for i in range(n-2):
-            out = threeSum(nums[i+1:], target-nums[i])
             
-            for item in out:
-                if [nums[i]] + list(item) not in output:
-                    output.append([nums[i]] + list(item))
-            
+            for j in range(i+1, n-1):
+                k = j + 1
+                l = n - 1
                 
-        return output
+                while k < l:
+                    sum_ = nums[i] + nums[j] + nums[k] + nums[l]
+                    
+                    if sum_ == target:
+                        answer.add((nums[i], nums[j], nums[k], nums[l]))
+                        k += 1
+                    elif sum_ > target:
+                        l -= 1
+                    else:
+                        k += 1
+                        
+        return list(answer)
+                
+                
+            
