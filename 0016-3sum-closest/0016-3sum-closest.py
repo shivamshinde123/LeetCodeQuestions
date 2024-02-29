3ea697e44490909cafa6777bcf4a98af0861a76e
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-02-29 14:26:01 +0530

    Time: 411 ms (53.76%), Space: 16.6 MB (74.04%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,9 +1,35 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         
+        # Approach 1: using dict
+#         n = len(nums)
+#         nums.sort()
+#         diff_to_sums = {}
+        
+#         for i in range(n-1):
+            
+#             j = i + 1
+#             k = n - 1
+            
+#             while (j < k):
+#                 sum_ = nums[i] + nums[j] + nums[k]
+                    
+#                 if sum_ < target:
+#                     j += 1
+#                 elif sum_ > target:
+#                     k -= 1
+#                 else:
+#                     j += 1
+                    
+#                 diff = abs(sum_ - target)
+#                 diff_to_sums[diff] = sum_
+                        
+#         return diff_to_sums[min(diff_to_sums.keys())]
+
+        # Approach 2
         n = len(nums)
         nums.sort()
-        diff_to_sums = {}
+        diff = math.inf
         
         for i in range(n-1):
             
@@ -12,6 +38,12 @@ class Solution:
             
             while (j < k):
                 sum_ = nums[i] + nums[j] + nums[k]
+                
+                local_diff = abs(sum_ - target)
+                
+                if local_diff < diff:
+                    diff = local_diff
+                    answer = sum_
                     
                 if sum_ < target:
                     j += 1
@@ -20,12 +52,6 @@ class Solution:
                 else:
                     j += 1
                     
-                diff = abs(sum_ - target)
-                diff_to_sums[diff] = sum_
                 
-            
-            
-                    
-        return diff_to_sums[min(diff_to_sums.keys())]
-
-                    
+                        
+        return answer
