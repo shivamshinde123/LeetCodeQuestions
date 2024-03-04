8d8fa67bef3b60d98321543d75dc1ce4d7519f31
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-03-04 15:14:57 +0530

    Time: 49 ms (25.02%), Space: 16.5 MB (52.35%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         
-        if len(nums) < 2:
+        if len(nums) == 1:
             return str(nums[0])
         
         
