45f660aa63fce52ecf8d256dbee4b7f12ec6bdd8
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-03-06 17:23:02 +0530

    Time: 42 ms (54.43%), Space: 16.8 MB (98.32%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,20 +1,26 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         
-        for i in range(len(citations), 0, -1):
+#         for i in range(len(citations), 0, -1):
             
-            val = 0
-            for citation in citations:
-                if citation >= i:
-                    val += 1
+#             val = 0
+#             for citation in citations:
+#                 if citation >= i:
+#                     val += 1
                     
-            if val >= i:
-                return i
+#             if val >= i:
+#                 return i
             
-        return 0 # When h_index is not valid for researcher i.e., researchers doesn't have any published work
+#         return 0 # When h_index is not valid for researcher i.e., researchers doesn't have any published work
     
     
-        
+        # Approach 2
+        citations.sort()
+        max_index = len(citations)
+        for i in citations:
+            if i<max_index:
+                max_index-=1
+        return max_index
             
                 
         
