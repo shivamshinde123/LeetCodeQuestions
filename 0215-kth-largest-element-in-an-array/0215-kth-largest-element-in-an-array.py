883de321bfd414e3981accce59d118934d0f45af
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-03-06 14:55:21 +0530

    Update 0215-kth-largest-element-in-an-array.py (manual change)

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -15,35 +15,45 @@ class Solution:
 #         kth_largest = heapq.heappop(nums)
 #         return kth_largest
 
-        # Approach 3: Using quickselect algorithm
-        def partition(nums, low, high):
-            pivot = nums[low]
-            i, j = low + 1, high
-            
-            while i <= j:
-                if nums[i] < pivot and pivot < nums[j]:
-                    nums[i], nums[j] = nums[j], nums[i]
-                    i += 1
-                    j -= 1
-                if nums[i] >= pivot:
-                    i += 1
-                if pivot >= nums[j]:
-                    j -= 1
-            
-            nums[low], nums[j] = nums[j], nums[low]
-            return j
+        # Approach 3: Using quickselect algorithm  (GETTING TIME LIMIT EXCEEDED ERROR, COULDN'T FIGURE OUT WHY)
+        # [arr[1]...arr[i]] -- Region 1 (less than or equal to pivot)
+        # [arr[i+1]...arr[j-1]] -- Region 2 (greater than pivot)
+        # [arr[j]...arr[n-1]] -- Unprocessed Region
+        # arr[n] -- pivot
         
-        low, high = 0, len(nums) - 1
-        pivotIndex = len(nums)
+        # Initially, Region1 and Region2 are empty and Unprocessed Region == Whole array and
+        # i = 0 and j = 1
         
-        while pivotIndex != k - 1:
-            pivotIndex = partition(nums, low, high)
-            if pivotIndex < k - 1:
-                low = pivotIndex + 1
+        def Partition(arr, leftIndex, rightIndex, pivotIndex):
+            pivot = nums[pivot_index]
+            nums[pivot_index], nums[rightIndex] = nums[rightIndex], nums[pivot_index]
+            stored_index = leftIndex
+            for i in range(leftIndex, rightIndex): 
+                if nums[i] < pivot:
+                    nums[i], nums[stored_index] = nums[stored_index], nums[i]
+                    stored_index += 1
+            nums[rightIndex], nums[stored_index] = nums[stored_index], nums[rightIndex]
+            return stored_index
+
+
+        n = len(nums)
+        
+        if n < 2:
+            return nums[0]
+    
+        leftIndex, rightIndex = 0, n - 1
+        # kth largest == (n-k) smallest
+        index = n - k # index of (n-k)th smallest element
+    
+        while True:
+            pivot_index = random.randint(leftIndex, rightIndex)
+            new_pivot_index = Partition(nums, leftIndex, rightIndex, pivot_index)
+            if new_pivot_index == index:
+                return nums[new_pivot_index]
+            elif new_pivot_index > index:
+                right = new_pivot_index - 1
             else:
-                high = pivotIndex - 1
-        
-        return nums[k - 1]
+                left = new_pivot_index + 1
         
         
         
