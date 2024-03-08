499476c8bd2f4b6020d7479b215d66cf7954ad4c
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-03-08 15:18:24 +0530

    Time: 55 ms (21.21%), Space: 17.4 MB (59.89%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,36 +1,20 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        left = 0
-        right = len(nums) - 1
-        middle = 0
-
-        ## Finding the target
-        while left <= right:
-            middle = (left + right) // 2
-
-            if target == nums[middle]:
-                return middle
-
-            if target > nums[middle]:
-                left = middle + 1
+        
+        n = len(nums)
+        left_index, right_index = 0, n - 1
+        
+        while left_index <= right_index:
             
-            if target < nums[middle]:
-                right = middle - 1
-
-
-        """
-        If the target is not found then we will find the index in list where it fits perfectly 
-        """
-        # Approach 1(Longer)
-        # for i in range(len(nums)-1):
-        #     if nums[i] <= target < nums[i+1]:
-        #         return i + 1
+            mid_index = left_index + (right_index - left_index) // 2
+            mid_value = nums[mid_index]
+            
+            if mid_value == target:
+                return mid_index
+            elif mid_value < target:
+                left_index = mid_index + 1
+            else:
+                right_index = mid_index - 1
+                
         
-        # if target < nums[0]:
-        #     return 0
-        
-        # if target >= nums[-1]:
-        #     return len(nums)
-
-        # Approach 2(Shorter)
-        return left
+        return right_index + 1
