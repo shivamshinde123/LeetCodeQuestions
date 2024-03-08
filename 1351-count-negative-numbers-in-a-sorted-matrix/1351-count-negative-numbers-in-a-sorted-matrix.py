9742f93b218e329156f635d433f074ac765e923a
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-03-08 17:26:38 +0530

    Time: 110 ms (19.55%), Space: 17.7 MB (85.62%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
@@ -1,41 +1,37 @@
-/**
- * @param {number[][]} grid
- * @return {number}
- */
-var countNegatives = function(grid) {
-    
-    let countNeg = (arr) => {
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
         
-        /*
-        This function will find the index of first negative number in an array and using it will find the count of total
-        negative numbers in the same array. Note that at the end of while loop, left_index position will be the index of first
-        negative number in an array.
-        */
-        
-        let n = arr.length
-        let left_index = 0
-        let right_index = n - 1
-        
-        while (left_index <= right_index) {
+        def countNeg(arr):
             
-            let mid_index = Math.floor((left_index + right_index) / 2)
-            let mid_value = arr[mid_index]
+            """
+            This function will find the index of first negative number in an array and using it will find the count of total
+            negative numbers in the same array. Note that at the end of while loop, left_index position will be the index of first
+            negative number in an array.
+            """
             
-            if (mid_value >= 0){
-                left_index = mid_index + 1
-            }
-            else {
-                right_index = mid_index - 1
-            }
-        }
+            n = len(arr)
+            left_index, right_index = 0, n - 1
+            
+            total_negative = 0
+            
+            while left_index <= right_index:
+                
+                mid_index = left_index + (right_index - left_index) // 2
+                mid_value = arr[mid_index]
+                
+                if mid_value >= 0:
+                    left_index = mid_index + 1
+                
+                if mid_value < 0:
+                    right_index = mid_index - 1
+                    
+            if left_index <= n - 1:
+                return len(arr) - left_index
+            else:
+                return 0
         
-        return arr.length - left_index
-    }
-    
-    let answer = 0
-    for (let arr of grid){
-        answer += countNeg(arr)
-    }
-    
-    return answer
-};
+        answer = 0
+        for arr in grid:
+            answer += countNeg(arr)
+            
+        return answer
