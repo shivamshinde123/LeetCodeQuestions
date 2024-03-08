258e2c3df7d86ce3cbce64478d9292f23984c661
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-03-08 14:54:35 +0530

    Time: 53 ms (91.32%), Space: 51.7 MB (27.78%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.js
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.js
@@ -15,6 +15,10 @@ var maxFrequencyElements = function(nums) {
         }
     }
     
+    /*
+    The "..." syntax in JavaScript is called the spread syntax or spread operator. It allows an iterable (such as an array) to be expanded into individual elements. It is commonly used for concatenating arrays, passing arguments to functions, or copying array or object properties.
+    */
+    
     let max_freq = Math.max(...Object.values(freq))
 
     let total_freq = 0
