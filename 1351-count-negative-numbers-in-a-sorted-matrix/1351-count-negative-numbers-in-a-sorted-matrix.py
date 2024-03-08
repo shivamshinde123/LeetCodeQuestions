eec26ba85a90dac7dd5a57b3b3a91c0700057662
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-03-08 17:16:20 +0530

    Time: 93 ms (96.36%), Space: 17.9 MB (26.44%) - LeetHub

---
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
@@ -0,0 +1,34 @@
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        
+        def countNeg(arr):
+            
+            """
+            This function will find the index of first negative number in an array and using it will find the count of total
+            negative numbers in the same array. Note that at the end of while loop, left_index position will be the index of first
+            negative number in an array.
+            """
+            
+            n = len(arr)
+            left_index, right_index = 0, n - 1
+            
+            total_negative = 0
+            
+            while left_index <= right_index:
+                
+                mid_index = left_index + (right_index - left_index) // 2
+                mid_value = arr[mid_index]
+                
+                if mid_value >= 0:
+                    left_index = mid_index + 1
+                
+                if mid_value < 0:
+                    right_index = mid_index - 1
+                    
+            return len(arr) - left_index
+        
+        answer = 0
+        for arr in grid:
+            answer += countNeg(arr)
+            
+        return answer
