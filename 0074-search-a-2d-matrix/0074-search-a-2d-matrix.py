ce4a3b8caa392a95dd71315cff08b5b7935d03c8
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-03-13 17:07:10 +0530

    Time: 45 ms (61.47%), Space: 17.2 MB (40.12%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,29 +1,51 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-            
-        def bsearch(arr, target):
-            n = len(arr)
         
-            left, right = 0, n - 1
+        # Approach 1
+#         def bsearch(arr, target):
+#             n = len(arr)
+        
+#             left, right = 0, n - 1
 
-            while left <= right:
-                mid = (left + right) // 2
-                mid_value = arr[mid]
+#             while left <= right:
+#                 mid = (left + right) // 2
+#                 mid_value = arr[mid]
 
-                if mid_value == target:
-                    return True
+#                 if mid_value == target:
+#                     return True
 
-                if mid_value > target:
-                    right = mid - 1
+#                 if mid_value > target:
+#                     right = mid - 1
 
-                if mid_value < target:
-                    left = mid + 1
+#                 if mid_value < target:
+#                     left = mid + 1
 
-            return False
+#             return False
             
-        for arr in matrix:
-            if bsearch(arr, target):
+#         for arr in matrix:
+#             if bsearch(arr, target):
+#                 return True
+        
+#         return False
+
+        # Approach 2
+        
+        m, n = len(matrix), len(matrix[0]) # number of rows and columns
+        left, right = 0, m * n - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            mid_row, mid_col = divmod(mid, n)
+            
+            if matrix[mid_row][mid_col] == target:
                 return True
-        
+            
+            if matrix[mid_row][mid_col] < target:
+                left = mid + 1
+                
+            if matrix[mid_row][mid_col] > target:
+                right = mid - 1
+                
+                
         return False
         
