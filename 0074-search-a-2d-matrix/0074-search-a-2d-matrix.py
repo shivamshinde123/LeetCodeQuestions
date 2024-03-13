8d67e3113a982e40ec152ca5f8f89560918605ca
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-03-13 16:57:33 +0530

    Time: 30 ms (99.74%), Space: 17.1 MB (81.42%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,26 +1,29 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        search_lst = list()
-        
-        for lst in matrix:
-            search_lst.extend(lst)
             
-        n = len(search_lst)
+        def bsearch(arr, target):
+            n = len(arr)
         
-        left, right = 0, n - 1
-        
-        while left <= right:
-            mid = (left + right) // 2
-            mid_value = search_lst[mid]
+            left, right = 0, n - 1
+
+            while left <= right:
+                mid = (left + right) // 2
+                mid_value = arr[mid]
+
+                if mid_value == target:
+                    return True
+
+                if mid_value > target:
+                    right = mid - 1
+
+                if mid_value < target:
+                    left = mid + 1
+
+            return False
             
-            if mid_value == target:
+        for arr in matrix:
+            if bsearch(arr, target):
                 return True
-            
-            if mid_value > target:
-                right = mid - 1
-                
-            if mid_value < target:
-                left = mid + 1
-                
+        
         return False
-            
+        
