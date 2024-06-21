288fcb4e179d6d5de509574453a6f2b9126c90cf
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2024-06-21 20:59:41 +0530

    Time: 36 ms (49.27%), Space: 16.4 MB (95.05%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -6,22 +6,31 @@
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        def get_node(index):
-            current = head
-            for _ in range(index):
-                current = current.next
-            return current
+        # The length of the linked list is needed
+#         def get_node(index):
+#             current = head
+#             for _ in range(index):
+#                 current = current.next
+#             return current
         
-        def get_length():
-            current = head
-            length = 0
-            while current:
-                length += 1
-                current = current.next
-            return length
+#         def get_length():
+#             current = head
+#             length = 0
+#             while current:
+#                 length += 1
+#                 current = current.next
+#             return length
                 
-        length = get_length()
-        if length % 2 == 1:
-            return get_node(((length + 1) // 2) - 1)
-        else:
-            return get_node(length // 2)
+#         length = get_length()
+#         if length % 2 == 1:
+#             return get_node(((length + 1) // 2) - 1)
+#         else:
+#             return get_node(length // 2)
+        
+        # Without calculating the length of the linked list (tortoise and hare algorithm or slow and fast pointer approach)
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
