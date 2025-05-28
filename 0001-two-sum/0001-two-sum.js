7d9c1b3a5ebdf46f24196bcc08a007996dbcb34d
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2025-05-28 15:30:48 -0400

    Time: 2 ms (70.62%), Space: 55.5 MB (31.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,16 +4,31 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let n = nums.length;
-    let indices = [];
-    for (let i=0;i<n;i++){
-        for (let j=i+1;j<n;j++){
-            if (nums[i] + nums[j] == target){
-                indices.push(i);
-                indices.push(j);
-            }
-        }
-    }
 
-    return indices
+    // Brute force approach
+    // let n = nums.length;
+    // let indices = [];
+    // for (let i=0;i<n;i++){
+    //     for (let j=i+1;j<n;j++){
+    //         if (nums[i] + nums[j] == target){
+    //             indices.push(i);
+    //             indices.push(j);
+    //         }
+    //     }
+    // }
+
+    // return indices
+
+    // hashmap (value -> index)
+    let hashmap = new Map();
+
+    for (let i = 0; i< nums.length; i++){
+        let diff = target - nums[i]
+
+        if (hashmap.has(diff)){
+           return [hashmap.get(diff), i]
+        }
+
+       hashmap.set(nums[i], i)
+    }
 };
