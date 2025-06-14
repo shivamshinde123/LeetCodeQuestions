09c120aaf95da5b6d24dded81963da3f36f7258c
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2025-06-14 14:05:44 -0400

    Time: 19 ms (37.47%), Space: 22.8 MB (13.17%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,18 +1,36 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        # Approach 1: Time Limit Exceeded
+        # def isAnagram(s1, s2):
+        #     return sorted(s1.lower()) == sorted(s2.lower())
         
-        # Dictionary that keeps track of anagrams
-        ana = dict() # anagram_representative : anagram list
-        # for strs = ["eat","tea","tan","ate","nat","bat"], ana will be
-        # {'aet': ['eat', 'tea', 'ate'], 'ant': ['tan', 'nat'], 'abt': ['bat']}
-        
-        # Creating a dictionary of lists
-        for element in strs:
-            rearranged_ele = ''.join(sorted(element))
-            if rearranged_ele not in ana:
-                ana[rearranged_ele] = list()
-                ana[rearranged_ele].append(element)
-            else:
-                ana[rearranged_ele].append(element)
-                
-        return ana.values()
+        # result = list()
+
+        # for i, s1 in enumerate(strs):
+        #     temp = list()
+        #     temp.append(s1)
+
+        #     for j, s2 in enumerate(strs):
+        #         if i != j:
+        #             if isAnagram(s1, s2):
+        #                 temp.append(s2)
+
+        #     temp.sort()
+        #     if temp not in result:
+        #         result.append(temp)
+
+        # return result
+
+        # Approach 2
+        result = defaultdict(list)
+
+        for s in strs:
+            counter = [0] * 26
+
+            for c in s:
+                counter[ord(c) - ord("a")] += 1
+
+            result[tuple(counter)].append(s)
+
+        return list(result.values())
