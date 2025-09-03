770979f3563318595256d3b2f6c899239fef471a
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2025-09-03 09:37:33 -0400

    Time: 2 ms (31.63%), Space: 58.2 MB (65.97%) - LeetHub

---
+++ 2058-concatenation-of-array/2058-concatenation-of-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    let n = nums.length
+
+    let ans = new Array(2 * n)
+
+    for (let i=0; i<n; i++){
+        ans[i] = nums[i]
+        ans[i+n] = nums[i]
+    }
+
+    return ans
+};
