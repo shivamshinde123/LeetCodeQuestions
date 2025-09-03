728fd67c97c298956a35d5363548382b5e730a48
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2025-09-03 09:34:18 -0400

    Time: 0 ms (100%), Space: 17.9 MB (70.51%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.py
+++ 2058-concatenation-of-array/2058-concatenation-of-array.py
@@ -1,10 +1,9 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        ans = [0] * (2*n)
-        for i, num in enumerate(nums):
+        ans = [0] * 2*n
+
+        for i in range(len(nums)):
             ans[i] = ans[i+n] = nums[i]
-        
+
         return ans
-
-
