59966ab6c5766dec9ddc493554a734484f318ba8
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2025-09-05 20:30:16 -0400

    Time: 55 ms (18.81%), Space: 58.2 MB (5.42%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let initial_value = init
+    return {
+        increment: () => {
+            init += 1
+            return init
+        },
+        decrement: () => {
+            init -= 1
+            return init
+        },
+        reset: () => {
+            init = initial_value
+            return init
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
