8ea3adeb253a7bf1a91d8f771b145b19dadcc171
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2025-09-28 16:50:58 -0400

    Time: 3 ms (46.59%), Space: 19 MB (48.1%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,53 +1,14 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
 
-        # Appraoch 1- running time: 78ms
-        expectedNums = list(set(nums))
-        expectedNums = self.mergeSort(expectedNums)
-        nums[0 : len(expectedNums) + 1] = expectedNums
-        return len(expectedNums)
+        # Brute force approach:Use set to get the unique elements and then put them in the first k positions
 
-        # Approach 2 - running time: 79ms
-        # j = 1
-        # for i in range(1, len(nums)):
-        #     if nums[i] != nums[i - 1]:
-        #         nums[j] = nums[i]
-        #         j += 1
-        # return j
-
-    def merge_two_arr(self, arr1, arr2):
-        merged_arr = list()
-        i, j = 0, 0
-        l1, l2 = len(arr1), len(arr2)
-
-        while (i < l1) and (j < l2):
-            if arr1[i] <= arr2[j]:
-                merged_arr.append(arr1[i])
+        # Optimal approach
+        i, j = 0, 1
+        n = len(nums)
+        for j in range(1, n):
+            if nums[i] != nums[j]:
                 i += 1
-            else:
-                merged_arr.append(arr2[j])
-                j += 1
-
-        while i < l1:
-            merged_arr.append(arr1[i])
-            i += 1
-
-        while j < l2:
-            merged_arr.append(arr2[j])
-            j += 1
-
-        return merged_arr
-
-    def mergeSort(self, arr):
-        if len(arr) <= 1:
-            return arr
-
-        middle_index = len(arr) // 2
-        left_arr = arr[:middle_index]
-        right_arr = arr[middle_index:]
-
-        left_arr = self.mergeSort(left_arr)
-        right_arr = self.mergeSort(right_arr)
-
-        sorted_arr = self.merge_two_arr(left_arr, right_arr)
-        return sorted_arr
+                nums[i], nums[j] = nums[j], nums[i]
+        
+        return i + 1
