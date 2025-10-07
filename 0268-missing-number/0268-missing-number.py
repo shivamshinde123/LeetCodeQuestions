1affca3638ce16b5d500393a64679f10aee5d21e
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2025-10-07 18:53:48 -0400

    Time: 8 ms (28.92%), Space: 18.8 MB (34.26%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,29 +1,17 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        # Approach 1 (Linear Search)
-        # for i in list(range(len(nums)+1)):
-        #     if i not in nums:
-        #         return i
-        # return -1
+        
+        xor1 = 0
+        xor2 = 0
+        n = len(nums) 
+        for i in range(n):
+            xor1 = xor1 ^ nums[i] # xor of array elements
+            xor2 = xor2 ^ i  # xor of number from 1 to n + 1
 
-        ## Approach 2 (sum of numbers from 0 to n minus sum of numbers in given array)
-        # n = len(nums)
-        # sum_of_nums = sum(nums)
-        # return int(n*(n+1)/2- sum_of_nums)
+        xor2 = xor2 ^ n # xor of xor1 and xor2
 
-        ## Approach 3 (Binary Search)
-        left = 0
-        right = len(nums) - 1
-        middle = 0
+        return xor1 ^ xor2
 
-        nums = sorted(nums)
+        
 
-        while left <= right:
-            middle = (left + right)//2
-
-            if nums[middle] != middle:
-                right = middle - 1
-            else:
-                left = middle + 1
-
-        return left
+        
