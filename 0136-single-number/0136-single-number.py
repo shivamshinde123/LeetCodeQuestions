0a03fc36181c47ab23257bafe9af64828c769125
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2025-10-16 01:21:42 -0400

    Time: 15 ms (12.26%), Space: 19.9 MB (14.09%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,14 +1,14 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        
+        counts = dict()
 
-        # Approach 1
-        # for i in range(len(nums)):
-        #     num = nums[i]
-        #     nums.remove(num)
-        #     if num not in nums:
-        #         return num
-        #     nums.insert(i, num)
+        for i in range(len(nums)):
+            if nums[i] not in counts:
+                counts[nums[i]] = 1
+            else:
+                counts[nums[i]] += 1
 
-        # Approach 2
-        unique_nums = list(set(nums))
-        return 2*sum(unique_nums) - sum(nums)
+        for key in counts.keys():
+            if counts[key] != 2:
+                return key
