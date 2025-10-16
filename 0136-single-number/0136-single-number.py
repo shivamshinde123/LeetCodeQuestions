99acbf6b9bbd0fc46dbcb083f989813187a7d1d0
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2025-10-16 01:26:29 -0400

    Time: 7 ms (31.44%), Space: 19.6 MB (77.08%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,14 +1,22 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         
-        counts = dict()
+        # Approach 1: Using maps
+        # counts = dict()
 
-        for i in range(len(nums)):
-            if nums[i] not in counts:
-                counts[nums[i]] = 1
-            else:
-                counts[nums[i]] += 1
+        # for i in range(len(nums)):
+        #     if nums[i] not in counts:
+        #         counts[nums[i]] = 1
+        #     else:
+        #         counts[nums[i]] += 1
 
-        for key in counts.keys():
-            if counts[key] != 2:
-                return key
+        # for key in counts.keys():
+        #     if counts[key] != 2:
+        #         return key
+
+        # Approach 2: Using XOR
+        xor = 0
+        for item in nums:
+            xor ^= item
+
+        return xor
