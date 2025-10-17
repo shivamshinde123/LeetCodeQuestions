3ddd4e305c138aab4a9d71bfd06a41b9ebe9a966
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2025-10-16 23:18:11 -0400

    Time: 0 ms (100%), Space: 19.1 MB (23.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        # Brute Force Approach
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+
+        # hashmap
+        hashmap = dict()
+
+        for index, value in enumerate(nums):
+            if (target - value) in hashmap:
+                return [index, hashmap[target - value]]
+            
+            hashmap[value] = index
+
+        
