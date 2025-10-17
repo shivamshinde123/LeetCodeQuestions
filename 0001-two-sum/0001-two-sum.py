e34ca504444c3837f97c2e43aca67e8e0c8b61f5
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2025-10-16 22:56:47 -0400

    Time: 1775 ms (19.19%), Space: 18.3 MB (95.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,25 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Brute force approach
-        # n = len(nums)
-        # indices = list()
-        # for i in range(n):
-        #     for j in range(i+1,n):
-        #         if nums[i] + nums[j] == target:
-        #             indices.append(i)
-        #             indices.append(j)
-
-        # return indices
-
-        # HashMap (val -> index)
-
-        hashmap = dict()
-
-        for index, num in enumerate(nums):
-            diff = target - num
-
-            if diff in hashmap:
-                return [hashmap[diff], index]
-            
-            hashmap[num] = index
-
+        
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
