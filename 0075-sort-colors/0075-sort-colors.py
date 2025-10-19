b3903f0f4a2d275a94d84b5015451f06872c8401
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2025-10-18 21:18:28 -0400

    Time: 3 ms (12.62%), Space: 17.8 MB (54.35%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,14 +3,43 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        k = n - 1
-        while k >= 0:
-            for i in range(k):
-                if nums[i] > nums[i+1]:
-                    nums[i], nums[i+1] = nums[i+1], nums[i]
-            k -= 1
+        # BUBBLE SORT
+        # n = len(nums)
+        # k = n - 1
+        # while k >= 0:
+        #     for i in range(k):
+        #         if nums[i] > nums[i+1]:
+        #             nums[i], nums[i+1] = nums[i+1], nums[i]
+        #     k -= 1
         
-        return nums
+        # return nums
+
+        # QUICK SORT
+        def partition(arr, low, high):
+            pivot = arr[low]
+            i = low
+            j = high
+
+            while (i < j):
+                while (arr[i] <= pivot and i <= high-1):
+                    i +=1
+                while (arr[j] > pivot and j >= low):
+                    j -= 1
+
+                if (i < j):
+                    arr[i], arr[j] = arr[j], arr[i]
+            
+            arr[low], arr[j] = arr[j], arr[low]
+
+            return j
+
+        def quicksort(arr, low, high):
+            if (low < high):
+                pIndex = partition(arr, low, high)
+                quicksort(arr, low, pIndex-1)
+                quicksort(arr, pIndex+1, high)
+
+        n = len(nums)
+        quicksort(nums, 0, n-1)
 
         
