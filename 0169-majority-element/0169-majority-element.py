b883c231a961cf99e4f0093216554001917b2529
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2025-10-18 21:31:06 -0400

    Time: 4 ms (71.03%), Space: 19.5 MB (5.12%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,15 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        ## Insertion sort
-        for i in range(1, len(nums)):
-            anchor = nums[i]
-            j = i - 1
-            while j >= 0 and anchor < nums[j]:
-                nums[j+1] = nums[j]
-                j -= 1
-            
-            nums[j+1] = anchor
         
-        ## After sorting, majority element (elements that repeats itself atleast n//2+1 times)
-        ## lies at n//2 th place
-        return nums[math.floor(len(nums)/2)]
+        hashmap = Counter(nums)
+        n = len(nums)
+        for item in hashmap:
+            if hashmap[item] > (n // 2):
+                return item
