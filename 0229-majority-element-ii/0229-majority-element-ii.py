b24881fb8cceca4f195d0cae0ee76493d2ee33cf
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2025-11-11 23:02:48 -0500

    Time: 11 ms (30.49%), Space: 22.3 MB (37.09%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,26 +1,47 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        
+
         # Approach 1 - we can have maximum of 2 such elements
+        # n = len(nums)
+        # freq = dict()
+        # result = list()
+
+        # for i in range(n):
+        #     if nums[i] not in freq:
+        #         freq[nums[i]] = 1
+            
+        #     else: 
+        #         freq[nums[i]] += 1
+
+
+        # for item, value in freq.items():
+        #     if len(result) == 2: # break the loop when the length of result is equal to 2
+        #         break
+
+        #     if value > n // 3:
+        #         result.append(item)
+
+        # return result
+        
+        # Approach 2 - simplified version of approach 1
         n = len(nums)
         min = n // 3
         freq = dict()
         result = list()
 
         for i in range(n):
+            if len(result) == 2:
+                break 
+
             if nums[i] not in freq:
                 freq[nums[i]] = 1
             
             else: 
                 freq[nums[i]] += 1
 
-        print(freq)
-
-        for item, value in freq.items():
-            if len(result) == 2: # break the loop when the length of result is equal to 2
-                break
-
-            if value > n // 3:
-                result.append(item)
+            if nums[i] not in result and freq[nums[i]] > min:
+                result.append(nums[i])
 
         return result
+
+        
