80ac398c5da6c5d6472ed3a326c5aa329c2aacd8
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2025-11-11 23:19:22 -0500

    Time: 15 ms (10.42%), Space: 20.5 MB (76.67%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -24,23 +24,68 @@ class Solution:
         # return result
         
         # Approach 2 - simplified version of approach 1
+        # n = len(nums)
+        # min = n // 3
+        # freq = dict()
+        # result = list()
+
+        # for i in range(n):
+        #     if len(result) == 2:
+        #         break 
+
+        #     if nums[i] not in freq:
+        #         freq[nums[i]] = 1
+            
+        #     else: 
+        #         freq[nums[i]] += 1
+
+        #     if nums[i] not in result and freq[nums[i]] > min:
+        #         result.append(nums[i])
+
+        # return result
+
+        # Approach 2: Extended Moore's voting algorithm
         n = len(nums)
-        min = n // 3
-        freq = dict()
-        result = list()
+        cnt1, cnt2, ele1, ele2 = 0, 0, None, None
 
         for i in range(n):
-            if len(result) == 2:
-                break 
 
-            if nums[i] not in freq:
-                freq[nums[i]] = 1
+            if cnt1 == 0 and nums[i] != ele2:
+                cnt1 = 1
+                ele1 = nums[i]
+
+            elif cnt2 == 0 and nums[i] != ele1:
+                cnt2 = 1
+                ele2 = nums[i]
+
+            elif ele1 == nums[i]:
+                cnt1 += 1
+
+            elif ele2 == nums[i]:
+                cnt2 += 1
+
+            else:
+                cnt1 -= 1
+                cnt2 -= 1
+
+        cnt1, cnt2 = 0, 0
+
+        for i in range(n):
+            if nums[i] == ele1:
+                cnt1 += 1
             
-            else: 
-                freq[nums[i]] += 1
+            if nums[i] == ele2:
+                cnt2 += 1
 
-            if nums[i] not in result and freq[nums[i]] > min:
-                result.append(nums[i])
+        min = n // 3
+
+        result = list()
+
+        if cnt1 > min:
+            result.append(ele1)
+
+        if cnt2 > min: 
+            result.append(ele2)
 
         return result
 
