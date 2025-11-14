a7e7564c7480b55fcfda4da139c65c7182ceab50
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2025-11-14 01:08:43 -0500

    Time: 574 ms (59.39%), Space: 20.8 MB (20.58%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,28 +1,50 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
-        answer = set()
+        # Brute force solution - O(N^3) - Time Limit Exceeded
+        # ans = list()
+        # n = len(nums)
+        # for i in range(n):
+        #     for j in range(i+1,n):
+        #         for k in range(j+1, n):
+        #             if nums[i] + nums[j] + nums[k] == 0 and sorted([nums[i], nums[j], nums[k]]) not in ans:
+        #                 ans.append(sorted([nums[i], nums[j], nums[k]]))
+
+        # return ans
+
+        # 3 pointer approach
         n = len(nums)
-        
         nums.sort()
-        
-        for i in range(n-1):
-            
-            j = i + 1
-            k = n - 1
-            
-            while j < k:
-                sum_ = nums[i] + nums[j] + nums[k]
+        ans = list()
+        for i in range(n):
+
+            # remove duplicates
+            if i != 0 and nums[i] == nums[i-1]:
+                    continue 
+
+            left = i + 1
+            right = n - 1
+
+            while left < right:
+
+                total_sum = nums[i] + nums[left] + nums[right]
                 
-                if sum_ == 0:
-                    answer.add((nums[i], nums[j], nums[k]))
-                    j += 1
-                elif sum_ > 0:
-                    k -= 1
+                if total_sum < 0:
+                    left += 1
+                
+                elif total_sum > 0:
+                    right -= 1
+
                 else:
-                    j += 1
-                
-        return list(answer)
-                
-            
+                    ans.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+
+                    # skipping the duplicate values
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
                     
+                    while left < right and  nums[right] == nums[right + 1]:
+                        right -= 1
+
+        return ans
