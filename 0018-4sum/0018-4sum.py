fd45d0f2767828f9e4c8154a57f65eb2ebdbce91
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2025-11-14 01:54:26 -0500

    Time: 371 ms (74.12%), Space: 17.7 MB (82.51%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,65 +1,43 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         
-        # Approach 1: Using threeSum method
-#         n = len(nums)
-#         output = list()
-#         nums.sort()
-        
-#         def threeSum(nums, target):
-#             answer = set()
-#             n = len(nums)
-            
-#             for i in range(n-1):
-
-#                 j = i + 1
-#                 k = n - 1
-
-#                 while j < k:
-#                     sum_ = nums[i] + nums[j] + nums[k]
-
-#                     if sum_ == target:
-#                         answer.add((nums[i], nums[j], nums[k]))
-#                         j += 1
-#                     elif sum_ > target:
-#                         k -= 1
-#                     else:
-#                         j += 1
-
-#             return list(answer)
-        
-#         for i in range(n-2):
-#             out = threeSum(nums[i+1:], target-nums[i])
-            
-#             for item in out:
-#                 if [nums[i]] + list(item) not in output:
-#                     output.append([nums[i]] + list(item))
-        
-#         return output
-    
-        # Approach 2: Intergrating threeSum into only one function
         n = len(nums)
-        answer = set()
+        ans = list()
         nums.sort()
-        
-        for i in range(n-2):
-            
-            for j in range(i+1, n-1):
-                k = j + 1
-                l = n - 1
-                
-                while k < l:
-                    sum_ = nums[i] + nums[j] + nums[k] + nums[l]
-                    
-                    if sum_ == target:
-                        answer.add((nums[i], nums[j], nums[k], nums[l]))
-                        k += 1
-                    elif sum_ > target:
-                        l -= 1
+        for i in range(n):
+
+            if i != 0 and nums[i] == nums[i - 1]:
+                continue
+
+            for j in range(i+1, n):
+
+                if j > i+ 1 and nums[j] == nums[j - 1]:
+                    continue
+
+                left = j + 1
+                right = n - 1
+
+                while left < right:
+
+                    total_sum = nums[i] + nums[j] + nums[left] + nums[right]
+
+                    if total_sum < target:
+                        left += 1
+
+                    elif total_sum > target:
+                        right -= 1
+
                     else:
-                        k += 1
-                        
-        return list(answer)
+                        ans.append([nums[i], nums[j], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+
+        return ans
+
                 
-                
-            
